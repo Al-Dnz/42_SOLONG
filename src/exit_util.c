f8b847f2fddd9c7a692142e72ea2ed5ea906408e
AlexDNZ <paleozene@gmail.com>
2021-09-03 17:11:52 +0200

    update free img method

--- src/exit_util.c
+++ src/exit_util.c
@@ -8,25 +8,25 @@ void	free_img(t_state *state)
 	while (i < 10)
 	{
 		if (state->number_img[i].mlx_img)
-			free(state->number_img[i].mlx_img);
+			mlx_destroy_image(state->mlx, state->number_img[i].mlx_img);
 		i++;
 	}
 	if (state->wall.mlx_img)
-		free(state->wall.mlx_img);
+		mlx_destroy_image(state->mlx, state->wall.mlx_img);
 	if (state->collectible.mlx_img)
-		free(state->collectible.mlx_img);
+		mlx_destroy_image(state->mlx, state->collectible.mlx_img);
 	if (state->player.mlx_img)
-		free(state->player.mlx_img);
+		mlx_destroy_image(state->mlx, state->player.mlx_img);
 	if (state->exit.mlx_img)
-		free(state->exit.mlx_img);
+		mlx_destroy_image(state->mlx, state->exit.mlx_img);
 	if (state->foe.mlx_img)
-		free(state->foe.mlx_img);
+		mlx_destroy_image(state->mlx, state->foe.mlx_img);
 	if (state->praticable.mlx_img)
-		free(state->praticable.mlx_img);
+		mlx_destroy_image(state->mlx, state->praticable.mlx_img);
 	if (state->win_screen.mlx_img)
-		free(state->win_screen.mlx_img);
+		mlx_destroy_image(state->mlx, state->win_screen.mlx_img);
 	if (state->gameover_screen.mlx_img)
-		free(state->gameover_screen.mlx_img);
+		mlx_destroy_image(state->mlx, state->gameover_screen.mlx_img);
 }
 
 int	quit(t_state *state, int error)
@@ -34,9 +34,9 @@ int	quit(t_state *state, int error)
 	int	fd;
 
 	fd = 1;
-	if (state->mlx)
-		mlx_destroy_image(state->mlx, state->img.mlx_img);
-	free_img(state);
+	mlx_destroy_image(state->mlx, state->img.mlx_img);
+	mlx_clear_window(state->mlx, state->win);
+	mlx_destroy_window(state->mlx, state->win);
 	free_map(state->map);
 	ft_strclr(&state->str_score);
 	ft_putstr_fd("EXIT\n", fd);
